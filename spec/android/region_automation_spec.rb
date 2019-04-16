53d788cc807c50ed53ac253b31fab5c9bf6258ee
Dharmendra Jadon <dharmendra@gogovan.co.in>
2019-04-16 08:45:46 +0530

    Added Region Automation Test

---
+++ spec/android/region_automation_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../../spec/spec_helper'
+
+desired_caps = {
+  caps: {
+    platformName: 'Android',
+    platformVersion: '9.0',
+    deviceName: 'emulator-5554',
+    app: ANDROID_APP,
+    automationName: 'UIAutomator2',
+    appActivity: 'hk.gogovan.clientapp.RootActivity'
+  }
+}
+
+describe 'Basic Android interactions' do
+
+  before(:all) do
+    @driver = Appium::Driver.new(desired_caps, false).start_driver
+  end
+
+  after(:all) do
+    @driver.quit
+  end
+
+  it 'Wait for Transport page' do
+    wait_true { !exists { @driver.find_element :id, 'topSheet' } }
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'appium_lib'
+require 'test/unit'
+extend Test::Unit::Assertions
+
+ANDROID_APP = '../apps/debug.apk'.freeze
+ANDROID_PACKAGE = 'hk.gogovan.clientapp'.freeze
